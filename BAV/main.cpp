ee97b7c9d7f7890836e12e18309122e532d62e5d
BudgetArms <67414205+BudgetArms@users.noreply.github.com>
2026-02-13 16:15:06 +0100

    Added GLFW in main.cpp & CMake

--- BAV/main.cpp
+++ BAV/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
     std::cout << "Hello World!" << "\n";
+
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW!" << "\n";
+        return 1;
+    }
 
     return 0;
 }
