5192d1bd0819cba61c25b8660d3a97c128a24f89
BudgetArms <67414205+BudgetArms@users.noreply.github.com>
2026-02-14 18:03:31 +0100

    Added GLM in CMakeLists

--- BAV/main.cpp
+++ BAV/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 int main()
 {
@@ -10,6 +11,9 @@ int main()
         std::cout << "Failed to initialize GLFW!" << "\n";
         return 1;
     }
+
+    glm::vec3 testGlm = glm::vec3(0.0f, 1.0f, 2.0f);
+    std::cout << testGlm.x << testGlm.y << testGlm.z << "\n";
 
     return 0;
 }
